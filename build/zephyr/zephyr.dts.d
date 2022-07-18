625ef5e97952f8bd2c87715cdf74a4492c44e482
Nicolas <niko722795@gmail.com>
2022-07-18 15:00:45 -0400

    First Commit

---
+++ build/zephyr/zephyr.dts.d
@@ -0,0 +1,14 @@
+empty_file.o: C:/nordicsemi/v2.0.0/zephyr/misc/empty_file.c \
+ C:/nordicsemi/v2.0.0/zephyr/boards/arm/nrf52840dk_nrf52840/nrf52840dk_nrf52840.dts \
+ C:/nordicsemi/v2.0.0/zephyr/dts/arm/nordic/nrf52840_qiaa.dtsi \
+ C:/nordicsemi/v2.0.0/zephyr/dts/common/mem.h \
+ C:/nordicsemi/v2.0.0/zephyr/dts/arm/nordic/nrf52840.dtsi \
+ C:/nordicsemi/v2.0.0/zephyr/dts/arm/armv7-m.dtsi \
+ C:/nordicsemi/v2.0.0/zephyr/dts/common/skeleton.dtsi \
+ C:/nordicsemi/v2.0.0/zephyr/dts/arm/nordic/nrf_common.dtsi \
+ C:/nordicsemi/v2.0.0/zephyr/include/zephyr/dt-bindings/i2c/i2c.h \
+ C:/nordicsemi/v2.0.0/zephyr/include/zephyr/dt-bindings/gpio/gpio.h \
+ C:/nordicsemi/v2.0.0/zephyr/include/zephyr/dt-bindings/pinctrl/nrf-pinctrl.h \
+ C:/nordicsemi/v2.0.0/zephyr/include/zephyr/dt-bindings/pwm/pwm.h \
+ C:/nordicsemi/v2.0.0/zephyr/dts/arm/nordic/override.dtsi \
+ C:/nordicsemi/v2.0.0/zephyr/boards/arm/nrf52840dk_nrf52840/nrf52840dk_nrf52840-pinctrl.dtsi
